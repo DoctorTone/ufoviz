945da67593f2c6193b71c24e46c3b918863cb9c4
DoctorTone <drtonesoftware@gmail.com>
2021-11-25 22:47:18 +0000

    Initial commit.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Canvas>
+        <ambientLight intensity={0.1} />
+        <directionalLight color="white" position={[5, 5, 5]} />
+        <mesh>
+          <boxBufferGeometry />
+          <meshStandardMaterial color="hotpink" />
+        </mesh>
+      </Canvas>
+    </>
   );
 }
 
